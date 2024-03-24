936897c93b5c091e9ea2bffb3bf632b536b532ea
Svitlana <svitlana.grytsai@gmail.com>
2024-03-24 17:54:57 +0100

    feat: implement formatDate function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -166,8 +166,15 @@ function isDateInPeriod(date, period) {
  * '1999-01-05T02:20:00.000Z' => '1/5/1999, 2:20:00 AM'
  * '2010-12-15T22:59:00.000Z' => '12/15/2010, 10:59:00 PM'
  */
-function formatDate(/* date */) {
-  throw new Error('Not implemented');
+function formatDate(date) {
+  const newDate = new Intl.DateTimeFormat('en-US', { timeZone: 'UTC' }).format(
+    new Date(date)
+  );
+  const newTime = new Intl.DateTimeFormat('en-US', {
+    timeStyle: 'medium',
+    timeZone: 'UTC',
+  }).format(new Date(date));
+  return `${newDate}, ${newTime}`;
 }
 
 /**
