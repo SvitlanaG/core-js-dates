3134b2d3ccc065c0bdb93eabda024191fc565b56
Svitlana <svitlana.grytsai@gmail.com>
2024-03-24 14:41:10 +0100

    feat: implement getNextFriday function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -86,8 +86,15 @@ function getDayName(date) {
  * Date('2024-02-13T00:00:00Z') => Date('2024-02-16T00:00:00Z')
  * Date('2024-02-16T00:00:00Z') => Date('2024-02-23T00:00:00Z')
  */
-function getNextFriday(/* date */) {
-  throw new Error('Not implemented');
+function getNextFriday(date) {
+  const myDate = new Date(date);
+  const dayOfTheWeek = myDate.getDay();
+  let diff = (5 - dayOfTheWeek + 7) % 7;
+  if (diff === 0) {
+    diff = 7;
+  }
+  myDate.setDate(myDate.getDate() + diff);
+  return myDate;
 }
 
 /**
