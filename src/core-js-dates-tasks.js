2531ed0193051ea2d8a22e3612e6c683062bb451
Svitlana <svitlana.grytsai@gmail.com>
2024-03-24 20:28:16 +0100

    feat: implement getNextFridayThe13th function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -229,8 +229,12 @@ function getWeekNumberByDate(/* date */) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  let tempFriday = getNextFriday(date);
+  while (tempFriday.getDate() !== 13) {
+    tempFriday = getNextFriday(tempFriday);
+  }
+  return tempFriday;
 }
 
 /**
